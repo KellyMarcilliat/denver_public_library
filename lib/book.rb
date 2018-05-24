f9892af05daffa2fba44d17440203fa138cac93e
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-05-24 10:20:38 -0600

    WIP - Iteration 1 complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,33 @@
 require 'pry'
 
 class Book
-  attr_reader :metadata
+  attr_reader :metadata,
+              :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date 
 
   def initialize(metadata)
     @metadata = metadata
+    @author_first_name = @metadata[:author_first_name]
+    @author_last_name = @metadata[:author_last_name]
+    @title = @metadata[:title]
+    @publication_date = @metadata[:publication_date]
   end
 
   def author_first_name
-    @author = @metadata[author_first_name]
-  end 
+    @author_first_name
+  end
+
+  def author_last_name
+    @author_last_name
+  end
+
+  def title
+    @title
+  end
+
+  def publication_date
+    @publication_date
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -6,6 +6,30 @@ class BookTest < Minitest::Test
     book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
     assert_instance_of Book, book
+  end
+
+  def test_book_has_author_first_name
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_equal "Harper", book.author_first_name
+  end
+
+  def test_book_has_author_last_name
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_equal "Lee", book.author_last_name
+  end
+
+  def test_book_has_title
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_equal "To Kill a Mockingbird", book.title
+  end
+
+  def test_book_has_publication_date
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_equal "July 11, 1960", book.publication_date
   end 
 
 end
