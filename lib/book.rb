8468b3a38f33d3fa51f01d4858d59ee0d32a2524
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-05-24 09:24:29 -0600

    Initial Commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,13 @@
+require 'pry'
+
 class Book
+  attr_reader :metadata
 
+  def initialize(metadata)
+    @metadata = metadata
+  end
 
+  def author_first_name
+    @author = @metadata[author_first_name]
+  end 
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,11 @@
 require './test/test_helper'
+require './lib/book'
 
 class BookTest < Minitest::Test
+  def test_book_object_exists
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
+    assert_instance_of Book, book
+  end 
 
 end
